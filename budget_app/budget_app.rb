e9ce18082584b66dcbdb85a1e0ab86394f624cbd
Nalipp <nateplipp@gmail.com>
2016-10-12 07:43:52 +0900

    view and sort

--- budget_app/budget_app.rb
+++ budget_app/budget_app.rb
@@ -15,69 +15,22 @@ configure do
 end
 
 before do
-  session[:categories] ||= []
   @storage = DataConnection.new(logger)
 end
 
-helpers do
-  def find_by(category)
-    session[:categories].select { |ctg| ctg[:category_type] == category }
-  end
-
-  def find_total(*category)
-    total = [0]
-    category.each do |category|
-      total << find_by(category).map { |ctg| ctg[:amount].to_i }.inject(:+)
-    end
-    total.compact.inject(:+)
-  end
-end
-
 get '/' do
-  redirect '/categories'
+  erb :summary, layout: :layout
 end
 
-def validate_invalid_length(*values)
-  !values.select { |value| value.length > 80 }.empty?
-end
-
-def validate_empty_value(*values)
-  !values.select { |value| value.length < 1 }.empty?
-end
-
-def find_next_id(category)
-  return 0 if find_by(category).empty?
-  find_by(category).last[:id]
-end
-
-get '/categories' do
-  erb :categories
-end
-
-get '/new_category/:category_type' do
-  @category_type = params[:category_type]
-  erb :new_category
-end
-
-post '/new_category/:category_type' do
-  @date = params[:date]
-  @amount = params[:amount]
-  @description = params[:description]
-  @category_type = params[:category_type]
-  @id = find_next_id(@category_type) + 1
-
-  if validate_invalid_length(@date, @amount, @description)
-    session[:error] = "Inputs must be less than 100 characters"
-    erb :new_category
-  elsif validate_empty_value(@amount)
-    session[:error] = "Amount must not be empty"
-    erb :new_category
+get '/all_money_subs/:sort_by' do
+  if params[:sort_by] == "category"
+    @all_money_subs = @storage.sort_by_category
+  elsif params[:sort_by] == "amount"
+    @all_money_subs = @storage.sort_by_amount
+  elsif params[:sort_by] == "description"
+    @all_money_subs = @storage.sort_by_description
   else
-    session[:categories] << { id: @id, date: @date, amount: @amount, description: @description, category_type: @category_type }
-    redirect "/new_category/#{@category_type}"
+    @all_money_subs = @storage.sort_by_date
   end
-
-  post '/new_category/:category_id' do
-
-  end
+  erb :money_subs, layout: :layout
 end
--- budget_app/data_connection.rb
+++ budget_app/data_connection.rb
@@ -4,4 +4,30 @@ class DataConnection
   def initialize(logger)
     DB.logger = logger
   end
+
+  def all_money_subs
+    DB[:money_subs].select { [money_subs__date_sub, money_subs__amount,
+                    money_subs__description, categories__category_type] }.
+                    join(:categories, id: :money_subs__category_id)
+  end
+
+  def convert_to_category_name(id)
+    DB[:categories].select(:category_type).where(id: id).first[:category_type]
+  end
+
+  def sort_by_category
+    all_money_subs.order(:category_type)
+  end
+
+  def sort_by_amount
+    all_money_subs.order(:amount)
+  end
+
+  def sort_by_date
+    all_money_subs.order(:date_sub)
+  end
+
+  def sort_by_description
+    all_money_subs.order(:description)
+  end
 end
